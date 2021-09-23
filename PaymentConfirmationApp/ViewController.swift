d988bb7c20258cacd75b2fb6de333eafa606002e
MizHiro <pencil3274@gmail.com>
2021-09-23 18:42:31 +0900

    Initial Commit

---
+++ PaymentConfirmationApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PaymentConfirmationApp
+//
+//  Created by mizoz on 2021/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
